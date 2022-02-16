640e3201b09703c0498dad9380c868fc6d67d398
Tasreen Mowreen <tmowreen97@gmail.com>
2022-02-16 18:42:47 -0500

    commit tree 312859989bcd19c5a4e97cfd46aa49cbc8d1e7a1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function isYear(array){
+    if (array.result === "W"){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function superbowlWin(array){
+    let winner = array.find(isYear);
+    if (winner){
+        return winner.year
+    }
+    else {
+        return undefined
+    }
+}
